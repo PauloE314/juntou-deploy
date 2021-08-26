71a4e83065f86f732b3968161446f6a9a2cc0b68
Paulo <paulo.eduardo@codeminer42.com>
2021-08-26 11:27:06 -0300

    DB configurations changes

--- src/config/database.js
+++ src/config/database.js
@@ -4,6 +4,11 @@ module.exports = {
     username: process.env.USERNAME || "postgres",
     password: process.env.PASSWORD || "docker",
     database: process.env.DATABASE || "juntouApp",
+    port: process.env.DB_PORT || 3306,
+    protocol: "postgres",
+    dialectOptions: {
+        ssl: true,
+    },
     define: {
         timestamps: true,
         underscored: true,
